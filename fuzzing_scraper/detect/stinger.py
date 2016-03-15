0af6c98f236450fff9706ed8fe5525952374ad0a
VillanCh <villan188228725@gmail.com>
2016-03-15 11:15:23 +0800

    Find Output by v1ll4n

--- fuzzing_scraper/detect/stinger.py
+++ fuzzing_scraper/detect/stinger.py
@@ -3,10 +3,11 @@ import sys
 from req import Req
 import requests
 import time
-import bs4
+#import bs4
+import re
 
 class Stinger():
-    def __init__(self, Req):
+    def __init__(self, Req, re_partten = '[^<^>]*zzz.*zzuf.*zzz[^<^>]*'):
         self.req = Req
         self.method = Req.method
         self.url = Req.url
@@ -15,19 +16,24 @@ class Stinger():
         self.post_data = Req.post_data
         
         self.response = None
-        self.soup = None
+        
+        self.re = re_partten
+        self.output_points = []
         
     def detect(self):
         if self.get_web_data(): 
-            self.analyze()
+            ret = self.analyze()
+            self.output_points = self.output_points + ret
+            return self.output_points
+                
         
     def get_web_data(self):
         count = 0
         while True:
             try:
-                req = requests.request(method = self.method, url = self.url, cookies = self.cookies,
-                                   headers = self.headers, data = self.post_data,
-                                   timeout = 10)
+                self.response = requests.request(method = self.method, url = self.url, cookies = self.cookies,
+                                                headers = self.headers, data = self.post_data,
+                                                timeout = 10)
             except requests.exceptions.ReadTimeout:
                 count = count + 1
                 if count == 5:
@@ -38,12 +44,23 @@ class Stinger():
                     
         del count
         
-        try:
-            self.soup = bs4.BeautifulSoup(req.text)
-        except:
-            pass
         
-        if self.soup == None:
-            raise StandardError("[!] Empty soup!");
+    def analyze(self):
+        if self.response == None:
+            raise StandardError('[!] No Rresponse')
         
-        return True
+        content = self.response.text
+        in_tag = re.findall(pattern = self.re, string = content)
+        
+        if in_tag == []:
+            print '[!] No Find Any tag matched!'
+            return False
+        else:
+            return in_tag
+        
+
+class InTagDetective(object):
+    def __init__(self, string = '', stylet = '', pattern = ''):
+        self.target_str = stylet
+        self.stylet = stylet
+        self.pattern = pattern
--- fuzzing_scraper/detect/test.py
+++ fuzzing_scraper/detect/test.py
@@ -1,13 +1,15 @@
+"""
 import requests
 try:
     response = requests.request(method = 'get', url = 'http://freebuf.com', timeout = 10)
 except requests.exceptions.ReadTimeout:
     print 'Time out £¡'
 print response.text.encode('gbk', 'ignore')
-
+"""
 import bs4
+import re
 
-soup = bs4.BeautifulSoup(response.text)
-all_tag = soup.findAll(data=re.)
-for i in all_tag:
-    print i
+ret = '<body><p><img src = "zzz ^*(<>: zzuf _+ zzz"></p> </body>\n\n<body><p><img src = "zzz ^*(<>: zzuf _+ zzz"></p> </body>\n\n'
+contents = re.findall('[^<^>]*zzz.*zzuf.*zzz[^<^>]', ret)
+print contents
+
