682ddd8ad026eba0d7785de47b92935c63c74c07
unknown <villan188228725@gmail.com>
2016-03-23 19:59:37 +0800

    Stinger.py MultiStingerMaker & StingerPool by v1ll4n

--- fuzzing_scraper/detect/stinger.py
+++ fuzzing_scraper/detect/stinger.py
@@ -5,10 +5,16 @@ import requests
 import time
 #import bs4
 import re
+import threading
+import Queue
+
+
+result = Queue.Queue()
+
 
 class Stinger():
-    def __init__(self, Req, re_partten = '[^<]*zzz.*zzuf.*zzz[^>]*'):
-        self.req = Req
+    def __init__(self, Req_obj, re_partten = '[^<]*zzz.*zzuf.*zzz[^>]*'):
+        self.req = Req_obj
         self.method = Req.method
         self.url = Req.url
         self.cookies = Req.cookies
@@ -21,14 +27,9 @@ class Stinger():
         self.re = re_partten
         self.output_points = []
         
-    def detect(self):
-        if self.get_web_data(): 
-            ret = self.analyze()
-            self.output_points = self.output_points + ret
-            return self.output_points
                 
         
-    def get_web_data(self):
+    def __get_web_data(self):
         count = 0
         while True:
             try:
@@ -49,12 +50,13 @@ class Stinger():
         del count
         return self.response
         
-    def get_output_points_str(self):
+    def get_output_points_str_list(self):
         if self.response == None:
-            self.get_web_data()
+            self.__get_web_data()
             
         if isinstance(self.response, type(None)):
-            raise ValueError("[!] Error in get_web_data() , Response empty!")
+            raise ValueError("[!] Error in __get_web_data() , Response empty!")
+        
         
         content = self.response.text
         in_tag = re.findall(pattern = self.re, string = content)
@@ -81,3 +83,46 @@ class Stinger():
         else:
             return in_tag
         
+        
+class MultiStingerMaker():
+    def __init__(self, Req_list, re_partten = '[^<]*zzz.*zzuf.*zzz[^>]*'):
+        if not isinstance(Req_list, list):
+            raise ValueError('[!] Need a list , but get a %s' % Req_list)
+
+        self.stinger_list = []
+        ret = None
+        
+    def get_stingers(self):
+        for i in Req_list:
+            ret = Stinger(Req_obj = i, re_partten=re_partten)
+            stinger_list.append(ret)
+        return self.stinger_list
+
+class StingerPool():
+    def __init__(self, stingers = [], threads_num = 8):
+        if len(stingers) == 0:
+            raise ValueError('[!] stingers Mustn\'t be empty')
+    
+        self.joblist = Queue.Queue()
+        for i in stingers:
+            self.joblist.put(i)
+        self.threads_num = threads_num
+    
+    def __init_worker(self):
+        for _ in range(self.threads_num):
+            ret = threading.Thread(target=self.__worker)
+            ret.deamon = True
+            ret.start
+            
+    def __worker(self):
+        while True:
+            if self.joblist.empty():
+                pass
+            else:
+                with self.joblist.get() as stinger:
+                    output_list = stinger.get_output_points_str_list()
+                    for i in output_list:
+                        result.put(i)
+    
+    def execute(self):
+        self.__init_worker()
